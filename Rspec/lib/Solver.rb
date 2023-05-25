ba37811343907ef8a916784c9b142622f4ea92a3
codedit334 <oussama7elabdioui@gmail.com>
2023-05-25 14:46:26 +0100

    Run rubocop

--- Rspec/lib/Solver.rb
+++ Rspec/lib/Solver.rb
@@ -1,14 +1,13 @@
 class Solver
-    def factorial
-    end
+  def factorial; end
 
-    def reverse
-    end
+  def reverse; end
 
-    def fizzbuzz(num)
-        return 'fizzbuzz' if num % 15 == 0
-        return 'fizz' if num % 3 == 0
-        return 'buzz' if num % 5 == 0
-        return num
-    end
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 15).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num
+  end
 end
--- Rspec/spec/fizzbuzz_spec.rb
+++ Rspec/spec/fizzbuzz_spec.rb
@@ -1,23 +1,23 @@
-require './Rspec/lib/Solver'
+require './Rspec/lib/solver'
 
 describe 'fizzbuzz' do
-    context 'Given one number' do
-        solver = Solver.new()
-        it 'returns the fizzbuzz string as it is divisible by 3 and 5' do
-            number = 15
-            expect(solver.fizzbuzz(number)).to eq('fizzbuzz')
-        end
-        it 'returns the fizz string as it is divisible by 3' do
-            number = 3
-            expect(solver.fizzbuzz(number)).to eq('fizz')
-       end
-       it 'returns the fizz string as it is divisible by 5' do
-            number = 5
-            expect(solver.fizzbuzz(number)).to eq('buzz')
-       end
-       it 'returns the fizz string as it is not divisible by 5 nor 3' do
-            number = 7
-            expect(solver.fizzbuzz(number)).to eq(number)
-       end
-     end
+  context 'Given one number' do
+    solver = Solver.new
+    it 'returns the fizzbuzz string as it is divisible by 3 and 5' do
+      number = 15
+      expect(solver.fizzbuzz(number)).to eq('fizzbuzz')
+    end
+    it 'returns the fizz string as it is divisible by 3' do
+      number = 3
+      expect(solver.fizzbuzz(number)).to eq('fizz')
+    end
+    it 'returns the fizz string as it is divisible by 5' do
+      number = 5
+      expect(solver.fizzbuzz(number)).to eq('buzz')
+    end
+    it 'returns the fizz string as it is not divisible by 5 nor 3' do
+      number = 7
+      expect(solver.fizzbuzz(number)).to eq(number)
+    end
+  end
 end

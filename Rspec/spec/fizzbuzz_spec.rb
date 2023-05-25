a3e3fa7f7084afd971fdcadb7908318ea9ae9924
codedit334 <oussama7elabdioui@gmail.com>
2023-05-25 14:41:10 +0100

    Create fizzbuzz test: divisible by 5

--- Rspec/spec/fizzbuzz_spec.rb
+++ Rspec/spec/fizzbuzz_spec.rb
@@ -11,5 +11,9 @@ describe 'fizzbuzz' do
             number = 3
             expect(solver.fizzbuzz(number)).to eq('fizz')
        end
+       it 'returns the fizz string as it is divisible by 5' do
+            number = 5
+            expect(solver.fizzbuzz(number)).to eq('buzz')
+       end
      end
 end

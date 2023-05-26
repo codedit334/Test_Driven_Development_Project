29c8a851893911497501e373f32ce0b9d54b9205
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-05-26 06:55:32 +0000

    Add first test case

--- Rspec/spec/reverse_spec.rb
+++ Rspec/spec/reverse_spec.rb
@@ -2,5 +2,10 @@ require './Rspec/lib/solver'
 
 describe '#reverse' do
   context 'Given a string' do
+    solver = Solver.new
+    it 'returns the reverse string' do
+      string = 'Hello'
+      expect(solver.reverse(string)).to eq('olleH')
+    end
   end
 end
